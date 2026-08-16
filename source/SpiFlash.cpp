8c9ef18ffe75a70c0059c842f2b17b005bfa3ae0
Jon Carrier <jjcarrier@gmail.com>
2026-08-16 17:26:40 -0400

    Sorted command sets by MFR name

--- source/SpiFlash.cpp
+++ source/SpiFlash.cpp
@@ -498,16 +498,16 @@ static const SpiCmdData kCmds_Microchip[] = {
 static const CmdSet kCmdSets[] = {
     // { id, name,     parentId, cmds,            cmdCount }
     { 0x00, "Unspecified",   -1, kCmds_Common,    static_cast<uint16_t>(std::size(kCmds_Common))    },
-    { 0x1F, "Renesas",     0x00, kCmds_Renesas,   static_cast<uint16_t>(std::size(kCmds_Renesas))   },
-    { 0xEF, "Winbond",     0x00, kCmds_Winbond,   static_cast<uint16_t>(std::size(kCmds_Winbond))   },
-    { 0xC2, "Macronix",    0x00, kCmds_Macronix,  static_cast<uint16_t>(std::size(kCmds_Macronix))  },
-    { 0xC8, "GigaDevice",  0xEF, nullptr,         0                                                 },
     { 0xA1, "Adesto",      0x00, kCmds_Adesto,    static_cast<uint16_t>(std::size(kCmds_Adesto))    },
     { 0x01, "Cypress",     0x00, kCmds_Cypress,   static_cast<uint16_t>(std::size(kCmds_Cypress))   },
-    { 0x9D, "Issi",        0xEF, kCmds_Issi,      static_cast<uint16_t>(std::size(kCmds_Issi))      },
-    { 0x20, "Micron",      0x00, kCmds_Micron,    static_cast<uint16_t>(std::size(kCmds_Micron))    },
-    { 0xBF, "Microchip",   0xEF, kCmds_Microchip, static_cast<uint16_t>(std::size(kCmds_Microchip)) },
     { 0x04, "Fujitsu",     0x00, nullptr,         0                                                 },
+    { 0xC8, "GigaDevice",  0xEF, nullptr,         0                                                 },
+    { 0x9D, "Issi",        0xEF, kCmds_Issi,      static_cast<uint16_t>(std::size(kCmds_Issi))      },
+    { 0xC2, "Macronix",    0x00, kCmds_Macronix,  static_cast<uint16_t>(std::size(kCmds_Macronix))  },
+    { 0xBF, "Microchip",   0xEF, kCmds_Microchip, static_cast<uint16_t>(std::size(kCmds_Microchip)) },
+    { 0x20, "Micron",      0x00, kCmds_Micron,    static_cast<uint16_t>(std::size(kCmds_Micron))    },
+    { 0x1F, "Renesas",     0x00, kCmds_Renesas,   static_cast<uint16_t>(std::size(kCmds_Renesas))   },
+    { 0xEF, "Winbond",     0x00, kCmds_Winbond,   static_cast<uint16_t>(std::size(kCmds_Winbond))   },
 };
 static constexpr size_t kCmdSetCount = std::size(kCmdSets);
 
